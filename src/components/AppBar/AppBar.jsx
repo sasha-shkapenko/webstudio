7fc65b7e53827594a8fff4ed6423caca050aad78
Sasha Shkapenko <sasha.shkapenko@gmail.com>
2023-08-25 17:04:29 +0100

    adds styles to header

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,34 +1,34 @@
 import { Link } from "react-router-dom";
-import s from "./AppBar.module.css";
+import "./AppBar.scss";
+
 const AppBar = () => (
-  <div className={s.header}>
-    <nav className={s.nav}>
-      <Link to="/">
-        {" "}
-        <span class={s.accent}>Web</span>Studio
+  <div className="header">
+    <nav className="nav">
+      <Link className="logo" to="/">
+        <span className="accent">Web</span>Studio
       </Link>
-      <Link to="/">Studio</Link>
-      <Link to="/portfolio">Portfolio</Link>
-      <Link to="/contact">Contacts</Link>
+      <Link className="nav-item" to="/">
+        Studio
+      </Link>
+      <Link className="nav-item" to="/portfolio">
+        Portfolio
+      </Link>
+      <Link className="nav-item" to="/contact">
+        Contacts
+      </Link>
     </nav>
-    <ul>
-      <li class="contacts__item">
-        <a
-          class="contacts__link contacts__link--email"
-          href="mailto:info@devstudio.com"
-        >
-          <svg class="contacts__icon" width="16" height="12">
+    <ul className="contact-list">
+      <li class="contact-item">
+        <a class="contact" href="mailto:info@devstudio.com">
+          <svg class="contacts-icon" width="16" height="12">
             <use href="./images/symbol-defs.svg#icon-email"></use>
           </svg>
           info@devstudio.com
         </a>
       </li>
-      <li class="contacts__item">
-        <a
-          class="contacts__link contacts__link--phone"
-          href="tel:+380961111111"
-        >
-          <svg class="contacts__icon" width="11" height="16">
+      <li class="contact-item">
+        <a class="contact" href="tel:+380961111111">
+          <svg class="contacts-icon" width="11" height="16">
             <use href="./images/symbol-defs.svg#icon-smartphone"></use>
           </svg>
           +38 096 111 11 11
